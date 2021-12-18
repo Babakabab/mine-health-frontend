8b9d29fc7106bee64bda3048bc8f31a8c516c44a
Babak <babak.jahangiri@hemenis.com>
2021-12-18 16:27:12 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import {authRouteConfig} from './auth';
+import {initialUrl} from 'shared/constants/AppConst';
+import {Redirect} from 'react-router-dom';
+import Error403 from './errorPages/Error403';
+import React from 'react';
+import {errorPagesConfigs} from './errorPages';
+import {samplePagesConfigs} from './sample';
+
+const authorizedStructure = {
+  fallbackPath: '/signin',
+  unAuthorizedComponent: <Error403 />,
+  routes: [...samplePagesConfigs],
+};
+
+const unAuthorizedStructure = {
+  fallbackPath: initialUrl,
+  routes: authRouteConfig,
+};
+
+const anonymousStructure = {
+  routes: errorPagesConfigs.concat([
+    {
+      path: '/',
+      exact: true,
+      component: () => <Redirect to={initialUrl} />,
+    },
+  ]),
+};
+
+export {authorizedStructure, unAuthorizedStructure, anonymousStructure};
---
+++ src/pages/sample/Page1/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Box, Paper} from '@mui/material';
+
+const Page1 = () => {
+  return (
+    <>
+      <h2>Sample page 1</h2>
+      <Box sx={{ml: 20}}>You can kick start your app</Box>
+      <Paper elevation='1' style={{width: 100}}>
+        yo
+      </Paper>
+    </>
+  );
+};
+
+export default Page1;
